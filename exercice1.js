2d3765b0bf86f75b53813d791dbebacf5e63e862
Sylvie-B <unsolo@hotmail.fr>
2020-12-04 10:03:36 +0100

    création div, suppr div

--- exercice1.js
+++ exercice1.js
@@ -1,11 +1,15 @@
-let monDiv = .createElement('div');
+let monDiv = document.createElement('div');
+monDiv.innerHTML = "Ce div a été ajouté en utilisant javascript";
+
 
 document.getElementById('viewport').appendChild(monDiv);
 
+let newDiv = document.createElement("div");
+newDiv.style.backgroundColor = "red";
+newDiv.style.width ="100px";
+newDiv.style.height ="100px";
 
+document.getElementById("viewport").appendChild(newDiv);
 
-
-
-
-
-
+let span01 = document.getElementById("aSupprimer");
+span01.remove();
